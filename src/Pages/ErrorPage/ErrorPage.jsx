fee52d7ee3ea9e2f690b67743012ae6cf6db221b
SAKIBUL HUDA ONNAY <onnoy29@gmail.com>
2025-10-07 16:16:55 +0600

    Started dwsigning

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            Not found <br />
+            status code 404
+        </div>
+    );
+};
+
+export default ErrorPage;
